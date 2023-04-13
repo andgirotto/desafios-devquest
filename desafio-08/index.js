b2eb001a7b72f20fb6cc84bfe711f3d2db3724bf
Anderson Girotto <andersongirotto1@gmail.com>
2023-04-13 16:15:10 -0300

    adicionando o desafio-08 javascript

---
+++ desafio-08/index.js
@@ -0,0 +1,9 @@
+const mensageInvite = confirm ("Seja bem vindo(a), aventureiro(a)! Gostaria de pagar R$ 50,00 para passar a noite na nossa hospedagem?")
+
+
+
+if (mensageInvite){
+    alert("Ótimo! Nós temos as melhores camas de toda a região")
+}else{
+    alert("Que pena! Você parecia ser uma pessoa mais legal")
+}
