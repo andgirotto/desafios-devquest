3a1914223cfd2ff6e95d6b980e1aec8faa9b745d
Anderson Girotto <andersongirotto1@gmail.com>
2023-05-10 15:11:15 -0300

    adição do desafio 11

---
+++ desafio-11/index.js
@@ -0,0 +1,11 @@
+//1. fazer o addeventlist click no botão
+//2. quando eu clicar na seta, ela deve permanecer na seta laranja
+//3. no momento do click deve abrir a resposta da pergunta correspondente
+//4. ao clicar em outra seta de pergunta a respota atual se fechara e abrirá a do click correspondente.
+
+const setaResposta = document.getElementById('btn-seta')
+const mostrarResposta = document.querySelector('mostrar')
+
+setaResposta = addEventListener('click' , (){
+    
+})
